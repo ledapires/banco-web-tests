1ad0ad82128dea1aee6cd060842a8c7bd346273b
ledapires <ledanashira@yahoo.com.br>
2025-08-02 17:03:09 -0300

    Adicionei dados provenientes do fixtures e a forma de rodar o script de cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,8 +7,11 @@ describe('Login', () => {
 
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
+
     cy.get('#login-section > .btn').click()
 
     //Assert
@@ -18,12 +21,15 @@ describe('Login', () => {
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
     //Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('698574')
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.invalido.usuario)
+      cy.get('#senha').click().type(credenciais.invalido.senha)
+    })
+
     cy.contains('button', 'Entrar').click()
 
     //Assert
-    cy.get('.toast').should('have.test', 'Erro no login. Tente novamente.')
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
 
   })
 })
